4501374d26aa6c63e87e57ecc364979e4062cade
Cruz <henriquecc2006@gmail.com>
2022-07-02 15:28:47 -0300

    n consigo resolver o movimento

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+let bglinho = 1
 let divcal = document.getElementById('movs')
 let cavalo = document.getElementById('cavalop')
 function cal(){
-    divcal.setAttribute('style', 'visibility:visible');
-
+    if(bglinho = 1){
+        divcal.setAttribute('style', 'visibility:visible')
+        bglinho +1;
+    }else{
+        divcal.setAttribute('style', 'visibility:hidden')
+        bglinho -1;
+    }
 }
 function lc1(){
     console.log('algo')
-    document.getElementById('cavalop').setAttribute('style', 'position:static', 'margin-bottom:200px', 'margin-left: -100px')
+    document.getElementById('cavalop1').setAttribute('style', 'bottom: 300px', 'left: 2000px')
 }
