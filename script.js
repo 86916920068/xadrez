d3f56c8a47f246804dd749f65d8616c3242a3333
Cruz <henriquecc2006@gmail.com>
2022-07-01 22:11:16 -0300

    alterações01.07
    consegui montar as peças pretas. comecei a pensar nos movimentos mas ainda há erros
    
    Co-Authored-By: Lucas Vizeu <102694035+lucasss45@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,10 @@
+let divcal = document.getElementById('movs')
+let cavalo = document.getElementById('cavalop')
+function cal(){
+    divcal.setAttribute('style', 'visibility:visible');
+
+}
+function lc1(){
+    console.log('algo')
+    document.getElementById('cavalop').setAttribute('style', 'position:static', 'margin-bottom:200px', 'margin-left: -100px')
+}
